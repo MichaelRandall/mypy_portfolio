b7a3c556fc5bb0e3a419869ae6a4e1d14721f0ed
Michael Randall <m_ran66@hotmail.com>
2018-10-07 23:39:46 -0700

    code refactor change assignment operators and remove unnecessary variable declarations

--- challenges/fizzbuzz1.py
+++ challenges/fizzbuzz1.py
@@ -1,21 +1,18 @@
 #!/usr/local/bin/python
 
 numbers = input("Enter a random number of integer values seperated by a space: ")
-numlist = numbers.split()
-
 output_string = ''
-#num = 0
 
-for numstr in numlist:
+for numstr in numbers.split():
     num = int(numstr)
     if num % 3 == 0 and num % 5 == 0:
-        output_string = output_string + 'fizzbuzz '
+        output_string += 'fizzbuzz '
     elif num % 3 == 0:
-        output_string = output_string + 'fizz '
+        output_string += 'fizz '
     elif num % 5 == 0:
-        output_string = output_string + 'buzz '
+        output_string += 'buzz '
     else:
-        output_string = output_string + '' + str(num) + ' '
+        output_string += '' + str(num) + ' '
 
 
 

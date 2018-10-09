6c9b000c083b0ee0d7e8fcf65bd27f2452e1b80c
Michael Randall <m_ran66@hotmail.com>
2018-10-09 01:38:53 -0700

    created three different options for reversing a string

--- challenges/stringreverser.py
+++ challenges/stringreverser.py
@@ -1,7 +1,23 @@
 #!/usr/local/bin/python
 
-# takes input from a user and prints the reversed input to the screen
+# take input from a user and print it to the screen reversed
 
 # prompt user for input and assign input to a variable
+astring = input("Type a word or phrase enclosed in single or double quotes: ")
 
-# reverse the input and print to screen
+# reverse the input character by character and print to screen
+print(astring[::-1])
+
+# reverse only the words in the input and print to screen
+newstr = astring.split()
+newstr.reverse()
+print(' '.join(newstr))
+
+
+# use map function to reverse characters in a string
+def strrev(str):
+    return str[::-1]
+
+myitem = map(strrev,newstr)
+
+print(' '.join(myitem))

7fcb5c7ce9c6e29c298c13ebd485ae66fbc9635b
Michael Randall <m_ran66@hotmail.com>
2018-10-10 11:45:26 -0700

    added oddeven.py challenge and added psudo code to command_line_craps crapsgame class

---
+++ challenges/oddeven.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python
+
+# Overview: prompt user to input a string and for every word in the string check if the number of characters is even or odd
+
+# prompt user for input and take user input and assign it to a list
+userstr = input("Type a word, or a phrase seperated by spaces, and surrounded with quotes: ")
+
+# loop through list of words determining word length and if even or odd number of characters
+for word in userstr.split():
+    if len(word) % 2 == 0:
+        print("EVEN: " + word)
+    else:
+        print("ODD: " + word)

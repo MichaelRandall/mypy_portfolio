ead4dd1c4eb1170e9dcefb0c0d9107c12513521b
Michael Randall <m_ran66@hotmail.com>
2018-10-14 12:13:36 -0700

    added test directory that contains a number of tests for python language

---
+++ challenges/tests/lint.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+"""
+DOCSTRING: just lints
+EXPLAIN: This is an example of a module level docstring
+HOWTO: run the command 'pylint -v lint.py -r  y' from the same directory
+"""
+
+def print_test():
+    """
+    DOCSTRING: testing pylint
+    INPUT: NULL
+    OUTPUT: NULL
+    EXPLAIN: This is an example of a method level docstring
+    """
+
+    var_a = 1
+    var_b = 2
+    print(var_a)
+    print(var_b)
